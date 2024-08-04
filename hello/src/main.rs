bafbab7bdba63523dfc8bb013956e5a56fe332d6
MOGINodoka <umbrellaislandwa@gmail.com>
2024-08-04 07:16:07 +0000

    短絡評価・非短絡評価のテスト

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
-    let x:i32=10;
-    let y = 20;
-    let z = mul(x,y);
-    
-    println!("z = {z}");
+    println!("短絡評価");
+    println!("{}", a() || b());
+
+    println!("非短絡評価");
+    println!("{}", a() | b());
 }
 
-fn mul(x: i32, y:i32) -> i32 {
-    // Rustでは、関数の最後の値が返り値となる
-    // ; は不要(;はその式の値を利用しないという意味)
-    x * y
+fn a() -> bool {
+    println!("call a");
+    true
+}
+
+fn b() -> bool {
+    println!("call b");
+    true
 }
