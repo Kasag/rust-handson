c5cd6318a6fd838636d14d1611a325e3eea891ca
MOGINodoka <umbrellaislandwa@gmail.com>
2024-08-06 14:30:35 +0000

    if文

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
-    let x:i32=10;
-    let y = 20;
-    let z = mul(x,y);
-    
-    println!("z = {z}");
+    sumup(10);
+
+    println!("End");
 }
 
-fn mul(x: i32, y:i32) -> i32 {
-    // Rustでは、関数の最後の値が返り値となる
-    // ; は不要(;はその式の値を利用しないという意味)
-    x * y
+fn sumup(n: u64) -> u64 {
+    if n == 0 {
+        0
+    } else {
+        let sum = n + sumup (n-1);
+        println!("sumup({}) = {}", n, sum);
+        
+    }
 }
